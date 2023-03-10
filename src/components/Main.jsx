fac8b690b093764a755f04c252d16c5406689d24
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-03-10 09:50:33 +0900

    Mainコンポーネント作成

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,18 @@ import React from 'react';
 import './Main.css';
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <div className="app-main">
+      <div className="app-main-note-edit">
+        <input type="text" />
+        <textarea id="" placeholder="ノートに内容を記入"></textarea>
+      </div>
+      <div className="app-main-note-preview">
+        <h1 className="preview-title">タイトル</h1>
+        <div className="markdown-preview">ノート内容</div>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
